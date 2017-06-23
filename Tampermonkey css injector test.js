e3abfb7b6170167d3475caada84f4e10b2d739e3
kingcatdaan <facekip@gmail.com>
2017-06-23 10:27:54 +0200

    test 1

---
+++ Tampermonkey css injector test.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name     Discord Theme Changer
+// @namespace      AdsBypasser
+// @description    Bypass Ads
+// @copyright      2017+ Kingcatdaan
+// @version        1.0
+// @license        BSD
+// @homepageURL    https://kingcatdaan.github.io/betterdiscord-themes/
+// @supportURL     https://github.com/kingcatdaan/betterdiscord-themes/issues
+// @icon           https://kingcatdaan.github.io/betterdiscord-themes/Images/y7Kjp6q.png
+// @include  https://discordapp.com/*
+// @grant    GM_addStyle
+// ==/UserScript==
+
+GM_addStyle ( "                                     \
+    .app {
+    background-image: url("https://kingcatdaan.github.io/betterdiscord-themes/Images/kz1EmSY.png");\
+    background-size: cover !important;\
+    background-position: center;\
+    }                                               \
+" );
